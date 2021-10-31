39000dd408a8136122771360d6e7ead7ea1a795e
Dmitriy Marinyuk <mdv-devops@gmail.com>
2021-11-01 00:31:41 +0400

    init

---
+++ kubernetes/vagrant-provisioning/jenkins_server/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
+
+echo "[TASK 3] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 4] Set root password"
+echo -e "jenkins\njenkins" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "[TASK 5] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.16.16.100   kmaster.example.com     kmaster
+172.16.16.101   kworker1.example.com    kworker1
+172.16.16.102   kworker2.example.com    kworker2
+172.16.16.200   haproxy.example.com     haproxy
+EOF
+
+
